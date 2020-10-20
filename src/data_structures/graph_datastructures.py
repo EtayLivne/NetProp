0931cf43e5f8d02e7a6eda1284db0427a1afd998
Etay <etay.livne@gmail.com>
2020-10-20 16:22:42 +0300

    code kinda sorta works now

--- src/data_structures/graph_datastructures.py
+++ src/data_structures/graph_datastructures.py
@@ -1,8 +1,19 @@
 from dataclasses import dataclass
-import networkx as nx
+from functools import total_ordering
 
-@dataclass
+
+@dataclass(frozen=False)
+@total_ordering
 class Protein:
     id: int
-    liquid: float
-    rank: int
+    liquid: float = 0
+    rank: int = 0
+
+    def __hash__(self):
+        return self.id
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __lt__(self, other):
+        return self.liquid < other.liquid
--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import networkx as nx
 from copy import deepcopy
-from math import sqrt, pow
+from math import sqrt, pow, floor
 
 # TODO finish refactor of collection of functions to class
 # TODO maybe save both normalized and unnormalized edge weights?
 # TODO support multiprocessing, shouldn't be difficult (famous last words, and this is really more of a bonus)
 WEIGHT = "weight"
+DATA = 2
+from data_structures.graph_datastructures import Protein
 
 
 def graph_from_file(file_path):
@@ -14,10 +16,18 @@ def graph_from_file(file_path):
     edge_weight_index = 2
 
     graph = nx.DiGraph()
+    discovered_nodes_map = dict()
     with open(file_path, 'r') as handler:
         for line in handler.readlines():
             values = line.split()
-            graph.add_edge(values[source_node_index], values[target_node_index], weight=values[edge_weight_index])
+            for node_index in (values[source_node_index], values[target_node_index]):
+                if node_index not in discovered_nodes_map:
+                    discovered_nodes_map[node_index] = Protein(id=int(node_index))
+
+            graph.add_edge(discovered_nodes_map[values[source_node_index]],
+                           discovered_nodes_map[values[target_node_index]],
+                           weight=float(values[edge_weight_index]))
+    return graph
 
 
 def get_propagater(source, from_file=True):
@@ -26,55 +36,78 @@ def get_propagater(source, from_file=True):
 
 
 class Propagater:
+    _PREV_LIQUID_PROPERTY_KEY = "prev_liquid"
+
     def __init__(self, graph):
         self.graph = graph
-        self._liquid_snapshot = None
         self.gap = None
 
         self._normalize_edge_weights()
         self._reset_propagation_parameters()
 
     def _reset_propagation_parameters(self):
-        self._liquid_snapshot = dict()
         self.gap = 0
-        for node in self.graph.nodes():
+        for node in self.graph:
             node.liquid = 0
+            self._set_prev_liquid(node, 0)
 
     # assumes graph is directed, otherwise each edge will be normalized twice, which is bad!
     def _normalize_edge_weights(self):
         for node in self.graph:
-            sum_of_weights = sum(edge[WEIGHT] for edge in self.graph.out_edges(node))
-            for edge in self.graph.out_edges(node):
+            edges = [self.graph[node][neighbor] for neighbor in self.graph.neighbors(node)]
+            sum_of_weights = sum(edge[WEIGHT] for edge in edges)
+            for edge in edges:
                 edge[WEIGHT] = float(edge[WEIGHT]) / sum_of_weights
 
-    def _propagate_once(self, subgraph, confidence_coefficient, prior_set):
-        alfa = confidence_coefficient
-        self._liquid_snapshot = {node: node.liquid for node in subgraph.nodes()}
+    def _get_prev_liquid(self, node):
+        return self.graph.nodes[node][self._PREV_LIQUID_PROPERTY_KEY]
 
-        if next(iter(prior_set)).liquid == 0:
-            for node in prior_set:
+    def _set_prev_liquid(self, node, prev_liquid):
+        self.graph.nodes[node][self._PREV_LIQUID_PROPERTY_KEY] = prev_liquid
+
+    def _propagate_once(self, subgraph, prior_set_subgraph, confidence_coefficient):
+        alfa = confidence_coefficient
+        for node in subgraph:
+            self._set_prev_liquid(node, node.liquid)
+
+        if next(iter(prior_set_subgraph)).liquid == 0:
+            for node in prior_set_subgraph:
                 node.liquid = 1 - alfa
-                self.gap = sqrt(pow((1-alfa), 2) * len(prior_set))
+            self.gap = sqrt(pow((1-alfa), 2) * len(prior_set_subgraph))
         else:
             for node in subgraph.nodes():
-                if node in prior_set:
+                for neighbor in subgraph.neighbors(node):
+                    neighbor.liquid = neighbor.liquid + self._get_prev_liquid(node) * subgraph[node][neighbor][WEIGHT] * alfa
+                node.liquid = node.liquid - self._get_prev_liquid(node)
+                # pump new liquid for next round
+                if node in prior_set_subgraph:
                     node.liquid = node.liquid + (1 - alfa)
-                for neighbour in subgraph.neighbours(node):
-                    neighbour.liquid = neighbour.liquid + node.liquid * subgraph[node][neighbour][WEIGHT] * alfa
-                node.liquid = node.liquid - self._liquid_snapshot[node]
-            self.gap = sqrt(sum(pow(node.liquid - self._liquid_snapshot[node], 2) for node in subgraph.nodes()))
 
-    def propagate(self, confidence_coefficient, prior_set, halt_condition_gap=10e-4):
+            self.gap = sqrt(sum(pow(node.liquid - self._get_prev_liquid(node), 2) for node in subgraph.nodes()))
+
+    def propagate(self, confidence_coefficient, prior_set_ids, halt_condition_gap=10e-4):
         self._reset_propagation_parameters()
-        discovered_subgraph_nodes = prior_set
-        newest_subgraph_nodes = prior_set
+        prior_set = {node for node in self.graph.nodes if node.id in prior_set_ids}
+        prior_set_subgraph = nx.subgraph(self.graph, prior_set)
 
+        discovered_subgraph_nodes = set(prior_set)
+        newest_subgraph_nodes = set(prior_set)
         self.gap = 10*halt_condition_gap  # only important detail is that gap > halt_condition_gap
         while self.gap > halt_condition_gap:
             discovered_subgraph = nx.subgraph(self.graph, discovered_subgraph_nodes)
-            self._propagate_once(discovered_subgraph, confidence_coefficient, prior_set)
-            newest_subgraph_nodes = set.union(*{set(self.graph.neighbours(node)) for node in newest_subgraph_nodes})
-            discovered_subgraph_nodes.union(newest_subgraph_nodes)
+            self._propagate_once(discovered_subgraph, prior_set_subgraph, confidence_coefficient)
+            newest_subgraph_nodes = set.union(*[set(self.graph.neighbors(node)) for node in newest_subgraph_nodes])
+            discovered_subgraph_nodes.update(newest_subgraph_nodes)
 
     def top_k_candidates(self, k):
-        return sorted(self.graph.nodes(), reverse=True)[:k]
+        top_k = floor(len(self.graph) / k)
+        return sorted(self.graph.nodes(), reverse=True)[:top_k]
+
+if __name__ == "__main__":
+    ppi = graph_from_file(r"C:\studies\thesis\code\NetProp\data\H_sapiens.net")
+    print("graph created!")
+    prop = Propagater(ppi)
+    prop.propagate(0.9, {1}, halt_condition_gap=0.01)
+    print("propagation complete!")
+    top_10 = prop.top_k_candidates(10)
+    jh = 76
