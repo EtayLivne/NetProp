05d8cc281e97820cb1edd33f28865a8ed2a07523
Roy <royee.livne6@gmail.com>
2020-11-08 15:09:20 +0200

    in progress

--- data_extractors.py
+++ data_extractors.py
@@ -2,6 +2,8 @@ import ndex2.client
 import networkx as nx
 from mygene import MyGeneInfo
 import pandas as pd
+from itertools import chain
+NDEX_SERVER_URL = 'public.ndexbio.org'
 HEK293T_SARS_CoV_2__Network_id = "43803262-6d69-11ea-bfdc-0ac135e8bacf"
 KROGAN_NODE_ID = 0
 KROGAN_NODE_DATA = 1
@@ -20,7 +22,7 @@ cov_networkx = nice_cx_from_server.to_networkx(mode="default")
 """
 
 def acquire_cov_human_ppi(as_nx=True):
-    nice_cx_from_server = ndex2.create_nice_cx_from_server(server='public.ndexbio.org',
+    nice_cx_from_server = ndex2.create_nice_cx_from_server(server=NDEX_SERVER_URL,
                                                            uuid=HEK293T_SARS_CoV_2__Network_id)
     return nice_cx_from_server.to_networkx(mode="default") if as_nx else nice_cx_from_server
 
@@ -33,4 +35,26 @@ def acquire_cov_human_ppi_prior_set_ids(cov_human_ppi):
     return set(int(node_data[NCBI_NODE_ID]) for node_data in ncbi_query.values)
 
 
+
+
 def acquire_cov_huamn_ppi_target_set(cov_human_ppi):
+    foothold_to_target_map = dict()
+    interactions = [edge[2] for edge in cov_human_ppi.edges(data=True)]
+    for interaction in interactions:
+        foothold = interaction['name'].split()[-1]
+        target = interaction.get('Preys', None)
+        if not target:
+            continue
+        if foothold in foothold_to_target_map:
+            foothold_to_target_map[foothold].add(target)
+        else:
+            foothold_to_target_map[foothold] = {target}
+    all_footholds = set(foothold_to_target_map.keys())
+    all_targets = chain(target for target in foothold_to_target_map.values())
+    all_nodes = all_footholds.add(all_targets)
+    ncbi_query = pd.DataFrame(MyGeneInfo().querymany(list(all_nodes), scopes="symbol", fields="entrezgene", species="human"))
+    jhghfgd = 9
+
+cov_human_ppi = acquire_cov_human_ppi()
+#acquire_cov_human_ppi_prior_set_ids(cov_human_ppi)
+acquire_cov_huamn_ppi_target_set(cov_human_ppi)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import networkx as nx
 from copy import deepcopy
 from math import sqrt, pow, floor
+from collections.abc import Iterable
+from itertools import chain
 
 # TODO finish refactor of collection of functions to class
 # TODO maybe save both normalized and unnormalized edge weights?
@@ -9,6 +11,8 @@ WEIGHT = "weight"
 DATA = 2
 from data_structures.graph_datastructures import Protein
 
+
+# TODO optimize away the .get in liquid spilling by initializing the gap keys for all nodes with 0 value for prior set nodes
 
 def graph_from_file(file_path):
     source_node_index = 0
@@ -36,20 +40,30 @@ def get_propagater(source, from_file=True):
 
 
 class Propagater:
-    _PREV_LIQUID_PROPERTY_KEY = "prev_liquid"
+    _PREV_LIQUID_PROPERTY_KEY = "prev_liquids"
+    _DEFUALT_HALT_CONDITION_GAP = 10e-3
 
-    def __init__(self, graph):
+    def __init__(self, graph, confidence_coefficient, halt_condition_gap=_DEFUALT_HALT_CONDITION_GAP):
         self.graph = graph
-        self.gap = None
+        self.confidence_coefficient = confidence_coefficient
+        self.halt_condition_gap = halt_condition_gap
+        self.gaps = None
 
         self._normalize_edge_weights()
         self._reset_propagation_parameters()
 
+    @staticmethod
+    def validate_propagation_node_sets(network, *node_id_sets):
+        missing_nodes = [n_id for n_id in chain(*node_id_sets) if n_id not in [node.id for node in network.nodes()]]
+        if missing_nodes:
+            print(f'The following nodes are specified in propagation node_set parameters but are not present in '
+                  f'the propagation network: {missing_nodes}')
+
     def _reset_propagation_parameters(self):
-        self.gap = 0
+        self.gaps = {node: 0 for node in self.graph}
         for node in self.graph:
             node.liquid = 0
-            self._set_prev_liquid(node, reset=True)
+            self._reset_liquid(node)
 
     # assumes graph is directed, otherwise each edge will be normalized twice, which is bad!
     def _normalize_edge_weights(self):
@@ -60,24 +74,25 @@ class Propagater:
                 edge[WEIGHT] = float(edge[WEIGHT]) / sum_of_weights
 
     def __get_node_properties(self, node):
-        return self.grap.nodes[node]
+        return self.graph.nodes[node]
 
-    def _get_prev_liquid(self, node, source_node):
-        node_properties = self.__get_node_properties(self, node)
-        return node_properties[self._PREV_LIQUID_PROPERTY_KEY][source_node]
-
-    def _set_prev_liquid(self, node, prev_liquid, source_node):
-        node_properties = self.__get_node_properties(self, node)
-        node_properties[self._PREV_LIQUID_PROPERTY_KEY][source_node] = prev_liquid
-
-    def _reset_liquid(self, node, source_nodes=None):
+    def _get_prev_liquid(self, node, liquid_source):
         node_properties = self.__get_node_properties(node)
-        if source_nodes:
-            for source_node in source_nodes:
-                del(node_properties[self._PREV_LIQUID_PROPERTY_KEY][source_node])
+        return node_properties[self._PREV_LIQUID_PROPERTY_KEY].get(liquid_source, 0)
+
+    def _set_prev_liquid(self, node, prev_liquid, liquid_source):
+        node_properties = self.__get_node_properties(node)
+        node_properties[self._PREV_LIQUID_PROPERTY_KEY][liquid_source] = prev_liquid
+
+    def _reset_liquid(self, node, liquid_sources=None):
+        node_properties = self.__get_node_properties(node)
+        if liquid_sources:
+            for liquid_source in liquid_sources:
+                del(node_properties[self._PREV_LIQUID_PROPERTY_KEY][liquid_source])
         else:
             node_properties[self._PREV_LIQUID_PROPERTY_KEY] = dict()
 
+    # Stores a snapshot of current liquid amount in each node into the node's prev_liquid property
     def _snapshot_to_prev_liquid(self, node):
         node_prev_liquids = self.__get_node_properties(node)[self._PREV_LIQUID_PROPERTY_KEY]
         node_prev_liquids.update(node.liquids)
@@ -87,44 +102,75 @@ class Propagater:
         for node in subgraph:
             self._snapshot_to_prev_liquid(node)
 
-        if next(iter(prior_set_subgraph)).liquid == 0:
+        # If liquids dict is empty in any node in prior set then this is the first propagation step
+        if not next(iter(prior_set_subgraph)).liquids:
             for node in prior_set_subgraph:
-                node.liquid = 1 - alfa
-            self.gap = sqrt(pow((1-alfa), 2) * len(prior_set_subgraph))
+                node.liquids[node] = 1 - alfa
+                self.gaps[node] = 1 - alfa
         else:
             for node in subgraph.nodes():
                 for neighbor in subgraph.neighbors(node):
-                    neighbor.liquid = neighbor.liquid + self._get_prev_liquid(node) * subgraph[node][neighbor][WEIGHT] * alfa
-                node.liquid = node.liquid - self._get_prev_liquid(node)
+                    edge_capacity = subgraph[node][neighbor][WEIGHT] * alfa
+                    for source_node in prior_set_subgraph:
+                        neighbor.liquids[source_node] = neighbor.liquids.get(source_node, 0) +\
+                                                        node.liquids.get(source_node, 0) * edge_capacity
+                for source_node in prior_set_subgraph:
+                    node.liquids[source_node] = node.liquids.get(source_node, 0) - self._get_prev_liquid(node, source_node)
                 # pump new liquid for next round
                 if node in prior_set_subgraph:
-                    node.liquid = node.liquid + (1 - alfa)
+                    node.liquids[node] = node.liquids[node] + (1 - alfa)
 
-            self.gap = sqrt(sum(pow(node.liquid - self._get_prev_liquid(node), 2) for node in subgraph.nodes()))
+            for source_node in prior_set_subgraph:
+                self.gaps[source_node] = sqrt(sum(pow(node.liquids[source_node] - self._get_prev_liquid(node, source_node), 2)
+                                                  for node in subgraph.nodes()))
 
-    def propagate(self, confidence_coefficient, prior_set_ids, halt_condition_gap=10e-4):
+    def propagate(self, prior_set_ids):
+        self.validate_propagation_node_sets(self.graph, prior_set_ids)
         self._reset_propagation_parameters()
         prior_set = {node for node in self.graph.nodes if node.id in prior_set_ids}
         prior_set_subgraph = nx.subgraph(self.graph, prior_set)
 
         discovered_subgraph_nodes = set(prior_set)
         newest_subgraph_nodes = set(prior_set)
-        self.gap = 10*halt_condition_gap  # only important detail is that gap > halt_condition_gap
-        while self.gap > halt_condition_gap:
+        # set gaps for all nodes in prior set to >> halt condition
+        self.gaps.update({node: 10*self.halt_condition_gap for node in prior_set})
+        while [v for v in self.gaps.values() if v > self.halt_condition_gap]:
             discovered_subgraph = nx.subgraph(self.graph, discovered_subgraph_nodes)
-            self._propagate_once(discovered_subgraph, prior_set_subgraph, confidence_coefficient)
+            self._propagate_once(discovered_subgraph, prior_set_subgraph, self.confidence_coefficient)
             newest_subgraph_nodes = set.union(*[set(self.graph.neighbors(node)) for node in newest_subgraph_nodes])
             discovered_subgraph_nodes.update(newest_subgraph_nodes)
 
+    # deprecated
     def top_k_candidates(self, k):
+        raise NotImplemented
         top_k = floor(len(self.graph) / k)
         return sorted(self.graph.nodes(), reverse=True)[:top_k]
 
+
+def measure_gene_knockout_impact(network_source_file_path, prior_set_ids, target_set_ids, knockout_candidate_ids):
+    if type(knockout_candidate_ids) is int:
+        knockout_candidate_ids = {knockout_candidate_ids}
+    elif not isinstance(knockout_candidate_ids, Iterable):
+        raise TypeError("knockoout_candidate_ids must be a single int or iterable of ints")
+    network = graph_from_file(network_source_file_path)
+    knockout_network = deepcopy(network)
+    knockout_network.remove_nodes_from(knockout_candidate_ids)
+    Propagater.validate_propagation_node_sets(network, prior_set_ids, target_set_ids, knockout_candidate_ids)
+
+    confidence_coefficient = 0.3
+    network_propagater = Propagater(network, confidence_coefficient)
+    knockout_network_propagater = Propagater(knockout_network, confidence_coefficient)
+
+    network_propagater.propagate(prior_set_ids)
+    knockout_network_propagater.propagate(prior_set_ids.difference(knockout_candidate_ids))
+    # TODO plot differences in graphs
+
+
+
 if __name__ == "__main__":
-    ppi = graph_from_file(r"C:\studies\thesis\code\NetProp\data\H_sapiens.net")
+    ppi = graph_from_file(r"..\data\H_sapiens.net")
     print("graph created!")
-    prop = Propagater(ppi)
-    prop.propagate(0.9, {1}, halt_condition_gap=0.01)
+    prop = Propagater(ppi, 0.9)
+    prop.propagate({1, 2, 100})
     print("propagation complete!")
-    top_10 = prop.top_k_candidates(10)
     jh = 76
