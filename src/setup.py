552accc06de13e2319435ce6a6dc383b941a1215
Etay <etay.livne@gmail.com>
2022-05-05 18:41:35 +0300

    updated version tag in setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netprop',
-    version='0.0.4.4',
+    version='0.0.5',
     packages=find_packages(),
     install_requires=[
         'matplotlib==3.5.1',
